040354a2ab39c7a3cfca8fa595c0143882927328
marcosalberto <159904717+marcosffp@users.noreply.github.com>
2024-06-23 20:12:11 -0300

    Update index.js

--- codigo/index.js
+++ codigo/index.js
@@ -4,13 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-// Configuração CORS para permitir solicitações do frontend específico
-const corsOptions = {
-  origin: 'https://frontend-cade-meu-pet.vercel.app',
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 /* Serve arquivos estáticos das pastas assets e data */
 app.use('/javascript', express.static(path.join(__dirname, 'javascript')));
